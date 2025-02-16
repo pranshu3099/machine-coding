7b5d99a3d861ab0131da22ecc78e07d425c86780
pranshu <srivastavapranshu2@gmail.com>
2025-02-16 20:50:54 +0530

    file explorer

--- src/fileexplorer.jsx
+++ src/fileexplorer.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import json from "./data.json";
 const FileExplorer = ({
   list,
   originalList,
@@ -8,57 +7,6 @@ const FileExplorer = ({
   removeNode,
 }) => {
   const [isexpanded, setIsExpanded] = useState({});
-  // const getParentId = (list, id, parentId = null) => {
-  //   for (let i = 0; i < list.length; i++) {
-  //     if (list[i].id === id) {
-  //       return parentId;
-  //     }
-  //     if (list[i]?.children) {
-  //       const result = getParentId(list[i].children, id, list[i].id);
-  //       if (result !== null) return result;
-  //     }
-  //   }
-  //   return null;
-  // };
-
-  // function updateFolder(list, parentId, updatedList) {
-  //   return list.map((item) => {
-  //     if (item.id === parentId) {
-  //       return { ...item, children: updatedList };
-  //     }
-  //     if (item.children) {
-  //       return {
-  //         ...item,
-  //         children: updateFolder(item.children, parentId, updatedList),
-  //       };
-  //     }
-  //     return item;
-  //   });
-  // }
-
-  // const handleRemove = (id, isFolder) => {
-  //   if (isFolder) {
-  //     const newList = originalList.filter((item) => {
-  //       return item.id !== id;
-  //     });
-  //     setList(newList);
-  //   } else {
-  //     let parentId = getParentId(originalList, id);
-  //     if (parentId === null) {
-  //       let newList = originalList.filter((item) => item.id !== id);
-  //       setList(newList);
-  //     } else {
-  //       const parentList = originalList.filter((item) => {
-  //         return item.id === parentId;
-  //       });
-  //       const updatedList = parentList?.[0]?.children?.filter((item) => {
-  //         return item.id !== id;
-  //       });
-  //       const updatedTree = updateFolder(originalList, parentId, updatedList);
-  //       setList(updatedTree);
-  //     }
-  //   }
-  // };
   return (
     <div className="container">
       {list.map((node) => (
